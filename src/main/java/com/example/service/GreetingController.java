40b058dd8b7fa373f214d3d6e6f7205a134ab525
Vladimir <16892@mail.ru>
2025-12-08 00:20:28 +0300

    Исправления для запуска сервиса

--- src/main/java/com/example/service/GreetingController.java
+++ src/main/java/com/example/service/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.demo1;
+package com.example.service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
